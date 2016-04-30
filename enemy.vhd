8f1b819a43d58ab8959142ae85e88c3999a51f77
hazrmard <ibrahim78786@gmail.com>
2016-04-30 16:15:02 -0500

    added random velocities

--- enemy.vhd
+++ enemy.vhd
@@ -54,12 +54,12 @@ component LFSR_GENERIC is			-- pseudo random number generator
 	end component LFSR_GENERIC;
 
   
-SIGNAL Ball_Y_motion, BAll_X_motion : std_logic_vector(9 DOWNTO 0);
+SIGNAL Ball_Y_motion, BAll_X_motion, ABS_X_motion, ABS_Y_motion : std_logic_vector(9 DOWNTO 0);
 SIGNAL Ball_Y_pos, Ball_X_pos		: std_logic_vector(9 DOWNTO 0);
 
 SIGNAL count: integer:=0;
 SIGNAL resetn, load: std_logic;
-SIGNAL random, random1, random2: std_logic_vector (9 downto 0);
+SIGNAL random, random1, random2, random3, random4: std_logic_vector (9 downto 0);
 SIGNAL seed: std_logic_vector (9 downto 0):="0000000" & s;
 
 
@@ -71,7 +71,7 @@ r1: LFSR_GENERIC port map (clock => vsync, resetn=>resetn, load=>load, serial_ou
 Initialize_Random: process (VSYNC)
 begin
 	
-	if count<5 then
+	if count<3 then
 		if count=0 then 		-- initialize random lookup table
 			resetn<='0';
 			load<='0';
@@ -81,19 +81,21 @@ begin
 		elsif count=2 then	-- get ready for generating random numbers
 			resetn<='1';
 			load<='0';
-		elsif count=3 then	-- get first random number
-			random1<=random;
-		elsif count=4 then	-- get second random number
-			random2<=random;
 		end if;
 		count <= count +1;
 	else					-- get newer random numbers
-		if count=5 then
+		if count=3 then
 			random1 <= random;
-			count<=6;
-		elsif count=6 then
+			count<=4;
+		elsif count=4 then
 			random2<= random;
 			count<=5;
+		elsif count=5 then
+			random3<= random;
+			count<=6;
+		elsif count=6 then
+			random4<= random;
+			count<=3;
 		end if;
 	end if;
 
@@ -101,6 +103,7 @@ end process;
 
 Move_Ball: process(RESET, VSYNC)--@@ Added sensitivity list
 	variable rint1, rint2: std_logic_vector (10 downto 0);
+	variable rint3, rint4: std_logic_vector (9 downto 0);
 	
 BEGIN
 
@@ -108,6 +111,8 @@ BEGIN
 	if RESET = '1' then		--initializing enemy positions and motions
 	rint1 := ('0' & conv_std_logic_vector((conv_integer(random1) mod 632), 10)) - ('0' & conv_std_logic_vector(316, 10));
 	rint2 := ('0' & conv_std_logic_vector((conv_integer(random2) mod 472), 10)) - ('0' & conv_std_logic_vector(236, 10));
+	rint3 := (conv_std_logic_vector((conv_integer(random2) mod 10), 10)) - (conv_std_logic_vector(5, 10));
+	rint4 := (conv_std_logic_vector((conv_integer(random2) mod 10), 10)) - (conv_std_logic_vector(5, 10));
 	
 		if rint1 < 0 then		-- left of center
 			Ball_X_pos <= rint1 (9 downto 0);		--removing sign bit
@@ -120,20 +125,23 @@ BEGIN
 		else						-- bottom of center
 			Ball_Y_pos <= conv_std_logic_vector(240,10) + rint2(9 downto 0);
 		end if;	
+		
+		Ball_X_motion <= rint3;
+		Ball_Y_motion <= rint4;
 	
 	elsif vsync'event and vsync='1' then
 		
 		-- Bounce off top or bottom of screen
 			IF ('0' & Ball_Y_pos) >= 472 THEN
-				Ball_Y_motion <= CONV_STD_LOGIC_VECTOR(-2,10);
+				Ball_Y_motion <= CONV_STD_LOGIC_VECTOR(0, 10) - Ball_Y_motion;
 			ELSIF Ball_Y_pos <= 8 THEN
-				Ball_Y_motion <= CONV_STD_LOGIC_VECTOR(2,10);
+				Ball_Y_motion <= CONV_STD_LOGIC_VECTOR(0, 10) - Ball_Y_motion;
 			END IF;
 			
 			IF('0' & Ball_X_pos) >= 632 THEN
-				Ball_X_motion <= CONV_STD_LOGIC_VECTOR(-2,10);
+				Ball_X_motion <= CONV_STD_LOGIC_VECTOR(0, 10) - Ball_X_motion;
 			ELSIF Ball_X_pos <= 8 THEN
-				Ball_X_motion <= CONV_STD_LOGIC_VECTOR(2,10);
+				Ball_X_motion <= CONV_STD_LOGIC_VECTOR(0, 10) - Ball_X_motion;
 			END IF;
 			
 			-- Compute next ball Y position
